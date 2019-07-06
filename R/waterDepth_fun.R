6db6fc72bb2ef3673cf48500bafc7b1fb1228f98
Nate Jones <njones@rstudio03.research.sesync.org>
2019-07-06 11:49:35 -0400

    PITA Date format

--- R/waterDepth_fun.R
+++ R/waterDepth_fun.R
@@ -8,13 +8,13 @@ waterDepth_fun<-function(#from working df
   #Organize Data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #Water height time series data
   ts<-tibble(Timestamp, waterHeight) %>% 
-    mutate(Timestamp = floor_date(Timestamp, "day")) %>%
+    mutate(Timestamp = as.Date(floor_date(Timestamp, "day"))) %>%
     group_by(Timestamp) %>%
     summarise(waterHeight = mean(waterHeight, na.rm = T))
   
   #Water depth at download (well log file)
   depths<-tibble(Timestamp = download_date, waterDepth=Relative_Water_Level_m) %>%
-    mutate(Timestamp = floor_date(as.POSIXct(Timestamp, tz = "America/New_York"), "day"))
+    mutate(Timestamp = as.Date(floor_date(as.POSIXct(Timestamp, tz = "America/New_York"), "day")))
   
   #Estimate water depth during each download~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   output<-left_join(depths, ts) %>%
